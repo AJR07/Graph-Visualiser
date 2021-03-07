bce96afaeb3bb50de3aa3e58b0946fe5c69df1e5
Ang Jun Ray <angjunray@live.com>
2021-03-07 11:06:40 +0800

    1D spring

--- index.ts
+++ index.ts
@@ -1,12 +1,23 @@
 import p5 from "p5";
 
+let y = 250, restLength = 200, k = 0.01, velocity = 0;
+
 new p5((p: p5) => {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.background(0);
   };
 
   p.draw = () => {
-    p.ellipse(p.mouseX, p.mouseY, 20);
+    p.background(0)
+    p.fill("blue");
+    p.circle(p.width / 2, y, p.width / 8);
+
+    let displacement = y - restLength;
+    let force = -k * displacement;
+
+    velocity += force;
+    y += velocity;
+
+    velocity *= 0.99;
   }
 });
