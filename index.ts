08502be40f88649f467c869ed4280db91b33ad24
Ang Jun Ray <angjunray@live.com>
2021-03-07 13:37:05 +0800

    tried to implement vectors into the spring

--- index.ts
+++ index.ts
@@ -1,23 +1,32 @@
 import p5 from "p5";
 
-let y = 250, restLength = 200, k = 0.01, velocity = 0;
+let position = p5.createVector(0, 5), anchor = [1], restLength = 200, springConstant = 0.01, velocity = [1];
 
+//sort out how to create those ^^ thingies as a p5 Vector type (ts doesn't allow or smth hm)
 new p5((p: p5) => {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    position = [300, 200];
+    anchor = [300, 0];
+    velocity = [0, 0];
   };
 
   p.draw = () => {
     p.background(0)
     p.fill("blue");
-    p.circle(p.width / 2, y, p.width / 8);
+    p.circle(anchor[0], position[1], 64)
+    p.circle(position[0], anchor[1], 32);
+    let force = p5.Vector.sub(p.createVector(position[0], position[1]), p.createVector(anchor[0], anchor[1]));
+    let displacement = v.mag();
+    force.normalize();
+    force.mult(-1 * springConstant * displacement);
 
-    let displacement = y - restLength;
-    let force = -k * displacement;
+    let velocity1 = p.createVector(velocity[0], velocity[1]);
+    velocity1.add(force);
 
-    velocity += force;
-    y += velocity;
+    let position1 = p.createVector(position[0], position[1]);
+    position1.add(velocity1);
 
-    velocity *= 0.99;
+    velocity1.mult(0.99);
   }
 });
